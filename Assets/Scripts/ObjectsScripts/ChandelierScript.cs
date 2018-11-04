34e653121287d9f2817fdf719c9125bc2a9873dc
EdouardBorissov <36777343+EdouardBorissov@users.noreply.github.com>
2018-11-03 20:25:01 -0500

    ObjectCode dunzo
    just gotta work on interactables now

---
+++ Assets/Scripts/ObjectsScripts/ChandelierScript.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChandelierScript : MonoBehaviour {
+    public bool isActive;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
--- Assets/Scripts/ObjectsScripts/ObjectsScript.cs
+++ Assets/Scripts/ObjectsScripts/ObjectsScript.cs
@@ -4,18 +4,58 @@ using UnityEngine;
 
 public class ObjectsScript : MonoBehaviour {
 
-    public bool isActivated;
+    private bool isActivated;
+  
+    public GameObject chairLeg;
+    public GameObject chandelier;
+    public GameObject barrelExplosion;
+    public GameObject flameWoosh;
+    public GameObject waterPuddle;
 
-	// Use this for initialization
-	void Start ()
-    {
-		
-	}
+
 	
 	// Update is called once per frame
 	void Update ()
     {
-		
+		if(isActivated)
+        {
+            isActivated = false;
+            if(gameObject.tag == "Chair")
+            {
+                Instantiate(chairLeg, transform.position, transform.rotation);
+                //do breaking animation here
+                //call Destroy at the end of the anim if it doesnt leave debris
+                Destroy(gameObject);
+            }
+            else if(gameObject.tag == "ChandelierRope")
+            {
+                chandelier.GetComponent<ChandelierScript>().isActive = true;
+                //do breaking animation here
+                //call Destroy at the end of the anim if it doesnt leave debris
+                Destroy(gameObject);
+            }
+            else if(gameObject.tag == "Fireplace")
+            {
+                Instantiate(flameWoosh, transform.position, transform.rotation);
+                //do breaking animation here
+                //call Destroy at the end of the anim if it doesnt leave debris
+
+            }
+            else if(gameObject.tag == "ExplosiveBarrel")
+            {
+                Instantiate(barrelExplosion, transform.position, transform.rotation);
+                //do breaking animation here
+                //call Destroy at the end of the anim if it doesnt leave debris
+                Destroy(gameObject);
+            }
+            else if(gameObject.tag == "WaterBucket")
+            {
+                Instantiate(waterPuddle, transform.position, transform.rotation);
+                //do breaking animation here
+                //call Destroy at the end of the anim if it doesnt leave debris
+                Destroy(gameObject);
+            }
+        }
 	}
 
     private void OnCollisionEnter2D(Collision2D col)
@@ -23,7 +63,7 @@ public class ObjectsScript : MonoBehaviour {
         
         if(col.gameObject.tag == "Interactable")
         {
-            
+            isActivated = true;
         }
 
     }
