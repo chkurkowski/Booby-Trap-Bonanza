9e8469b053ed0b85cf1181a6b71c2db4ceaa2901
Ty Loveland <tloveland98@gmail.com>
2018-11-06 12:47:51 -0600

    restart level function added
    Added keypress that allows player to reset the level

---
+++ Assets/restartGame.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class restartGame : MonoBehaviour 
+{
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            Application.LoadLevel(Application.loadedLevel);
+            print("reload successful");
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+}
