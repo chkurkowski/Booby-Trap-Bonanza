f8a1f64059b2b79ea2c01f180d97066387904426
EdouardBorissov <36777343+EdouardBorissov@users.noreply.github.com>
2018-11-13 13:17:24 -0600

    moreSmallChanges

---
+++ Assets/Scripts/TutorialLevelManagerScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TutorialLevelManagerScript : MonoBehaviour {
+    public GameObject triggerGoon;
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (triggerGoon == null)
+        {
+            SceneManager.LoadScene("ExampleLevel");
+        }
+	}
+}
